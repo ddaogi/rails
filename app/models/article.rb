1655168693d07e86f160a865f23d96fbbc395be3
ddaogi <slee855@wisc.edu>
2023-11-01 15:53:39 +0900

    feat: concerns 로 묶어서 관리하게끔 추가

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,16 @@
 class Article < ApplicationRecord
+  include Visible
+
+
   has_many :comments
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10}
+
+
+  #이걸 빼는 이유는 concerns로 관리하기 때문에
+  # VALID_STATUSES = ['public', 'private', 'archived']
+  # validates :status , inclusion: { in: VALID_STATUSES }
+  # def archived?
+  #   status == 'archived'
+  # end
 end
