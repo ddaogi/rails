69f9affe8b586b97ae3c4570c89fc3f8a084ab6c
ddaogi <slee855@wisc.edu>
2023-12-25 22:32:32 +0900

    feat: 다운로드 메소드추가, 환경설정 추가

--- app/models/geolocation.rb
+++ app/models/geolocation.rb
@@ -1,10 +1,34 @@
 require "redis"
 require "ipaddr"
 require "zlib"
+require "open-uri"
 class Geolocation
   class << self
+
+    def download_data
+      url = "https://ipinfo.io/data/free/country.csv.gz?token=#{ENV['IP_INFO_TOKEN']}"
+      file_path = Rails.root.join("public/country.json.gz").to_s
+      URI.open(url) do |remote_file|
+        File.open(file_path, "wb") do |file|
+          file.write(remote_file.read)
+        end
+      end
+    end
     def load_data
-      puts "load data called"
+
+      # URL에서 파일 다운로드
+
+
+      data = []
+      # 다운로드한 파일 처리
+      Zlib::GzipReader.open(file_path) do |gz|
+        gz.each_line do |line|
+          record = JSON.parse(line)
+          data << record
+        end
+      end
+      puts "data read done"
+
     end
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,18 @@ module Blog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    credentials.config.each do |key, value|
+      if key.to_s == Rails.env
+        value.each do |env_key, env_value|
+          ENV[env_key.to_s.upcase] = env_value.to_s if ENV[env_key.to_s.upcase].blank?
+          ENV[env_key.to_s.downcase] = env_value.to_s if ENV[env_key.to_s.downcase].blank?
+        end
+      elsif !%w[development staging test production].include?(key.to_s)
+        ENV[key.to_s.upcase] = value.to_s if ENV[key.to_s.upcase].blank?
+        ENV[key.to_s.downcase] = value.to_s if ENV[key.to_s.downcase].blank?
+      end
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
